63517423691b8be7d0c93bc229c956b91d3773c8
Alexander Litvinenko <a.litvinenko@synergetica.co>
2020-11-13 14:39:32 +0200

    plugin changes

--- broadpeakplugin/src/main/java/com/kaltura/playkit/plugins/broadpeak/BroadpeakConfig.java
+++ broadpeakplugin/src/main/java/com/kaltura/playkit/plugins/broadpeak/BroadpeakConfig.java
@@ -4,4 +4,32 @@ package com.kaltura.playkit.plugins.broadpeak;
  * Created by alex_lytvynenko on 04.11.2020.
  */
 public class BroadpeakConfig {
+
+    private String analyticsAddress;
+    private String nanoCDNHost;
+    private String broadpeakDomainNames;
+
+    public String getAnalyticsAddress() {
+        return analyticsAddress;
+    }
+
+    public void setAnalyticsAddress(String analyticsAddress) {
+        this.analyticsAddress = analyticsAddress;
+    }
+
+    public String getNanoCDNHost() {
+        return nanoCDNHost;
+    }
+
+    public void setNanoCDNHost(String nanoCDNHost) {
+        this.nanoCDNHost = nanoCDNHost;
+    }
+
+    public String getBroadpeakDomainNames() {
+        return broadpeakDomainNames;
+    }
+
+    public void setBroadpeakDomainNames(String broadpeakDomainNames) {
+        this.broadpeakDomainNames = broadpeakDomainNames;
+    }
 }
--- broadpeakplugin/src/main/java/com/kaltura/playkit/plugins/broadpeak/BroadpeakPlugin.java
+++ broadpeakplugin/src/main/java/com/kaltura/playkit/plugins/broadpeak/BroadpeakPlugin.java
@@ -1,10 +1,11 @@
 package com.kaltura.playkit.plugins.broadpeak;
 
 import android.content.Context;
-import android.os.Handler;
 
 import com.kaltura.playkit.BuildConfig;
 import com.kaltura.playkit.MessageBus;
+import com.kaltura.playkit.OnMediaInterceptorListener;
+import com.kaltura.playkit.PKError;
 import com.kaltura.playkit.PKLog;
 import com.kaltura.playkit.PKMediaConfig;
 import com.kaltura.playkit.PKMediaEntry;
@@ -12,6 +13,12 @@ import com.kaltura.playkit.PKMediaEntryInterceptor;
 import com.kaltura.playkit.PKMediaSource;
 import com.kaltura.playkit.PKPlugin;
 import com.kaltura.playkit.Player;
+import com.kaltura.playkit.PlayerEvent;
+import com.kaltura.playkit.player.PKPlayerErrorType;
+
+import tv.broadpeak.smartlib.SmartLib;
+import tv.broadpeak.smartlib.session.streaming.StreamingSession;
+import tv.broadpeak.smartlib.session.streaming.StreamingSessionResult;
 
 /**
  * Created by alex_lytvynenko on 04.11.2020.
@@ -19,7 +26,11 @@ import com.kaltura.playkit.Player;
 public class BroadpeakPlugin extends PKPlugin implements PKMediaEntryInterceptor {
     private static final PKLog log = PKLog.get("BroadpeakPlugin");
 
+    private final String SMARTLIB_PRE_STARTUP_TIME_KEY = "pre_startup_time";
     private Player player;
+    private MessageBus messageBus;
+    private StreamingSession session;
+    private long requestStartTime;
 
     public static final Factory factory = new Factory() {
         @Override
@@ -42,11 +53,23 @@ public class BroadpeakPlugin extends PKPlugin implements PKMediaEntryInterceptor
         }
     };
 
-
     @Override
     protected void onLoad(Player player, Object config, MessageBus messageBus, Context context) {
+        BroadpeakConfig bpConfig = (BroadpeakConfig) config;
+        SmartLib.getInstance().init(context,
+                bpConfig.getAnalyticsAddress(),
+                bpConfig.getNanoCDNHost(),
+                bpConfig.getBroadpeakDomainNames()
+        );
+        session = SmartLib.getInstance().createStreamingSession();
+
+        this.messageBus = messageBus;
         this.player = player;
         this.player.addMediaEntryInterceptor(this);
+
+        session.attachPlayer(this.player);
+
+        requestStartTime = System.currentTimeMillis();
     }
 
     @Override
@@ -67,21 +90,40 @@ public class BroadpeakPlugin extends PKPlugin implements PKMediaEntryInterceptor
 
     @Override
     protected void onDestroy() {
-        player.addMediaEntryInterceptor(this);
+        // Stop the session
+        session.stopStreamingSession();
+
+        player.removeMediaEntryInterceptor(this);
+        player = null;
+
+        // Release SmartLib
+        SmartLib.getInstance().release();
     }
 
     @Override
     public void apply(PKMediaEntry mediaEntry, OnMediaInterceptorListener listener) {
-        for (PKMediaSource source : mediaEntry.getSources()) {
-            source.setUrl("http://example.com");
+        // Set the pre-startup time
+        SmartLib.getInstance().setCustomParameter(SMARTLIB_PRE_STARTUP_TIME_KEY,
+                (System.currentTimeMillis() - requestStartTime) + "");
+
+        if (mediaEntry.getSources().size() > 0) {
+            PKMediaSource source = mediaEntry.getSources().get(0);
+            // Start the session and get the final stream URL
+            StreamingSessionResult result = session.getURL(source.getUrl());
+            if (!result.isError()) {
+                // Replace the URL
+                source.setUrl(result.getURL());
+            } else {
+                // send event to MessageBus
+                messageBus.post(new PlayerEvent.Error(
+                        new PKError(
+                                PKPlayerErrorType.LOAD_ERROR,
+                                result.getErrorMessage(),
+                                new Throwable(result.getErrorMessage()))
+                ));
+            }
         }
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        listener.onApplyMediaCompleted(null);
+        listener.onApplyMediaCompleted();
     }
 }
